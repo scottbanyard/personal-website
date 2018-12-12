f9ed822715648390b32d60a8ee79aa8a129590e6
Scott Banyard <scottbanyard1@yahoo.co.uk>
2018-12-12 21:05:04 +0000

    Added git hooks and tslint.

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
         path: path.join(__dirname, "dist")
     },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
@@ -19,9 +16,6 @@ module.exports = {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     }
 };
